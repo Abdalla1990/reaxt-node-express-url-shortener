4c0135eaf481d8f4967ea221ed2f930221a2c409
Abdalla <motmm0500@hotmail.com>
2019-08-03 15:53:19 -0400

    trying

--- db/credentials.js
+++ db/credentials.js
@@ -7,12 +7,6 @@ dev = {
     port: process.env.RDS_PORT || '5432',
 }
 
-prod = {
-  host     : process.env.RDS_HOSTNAME,
-  user     : process.env.RDS_USERNAME,
-  password : process.env.RDS_PASSWORD,
-  port     : process.env.RDS_PORT
-}
 module.exports = {
     dev
 }
