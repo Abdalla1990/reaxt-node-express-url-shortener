a8eed5e9532506d55f08383c25f411f6b17b2de8
Abdalla <motmm0500@hotmail.com>
2019-08-04 12:52:40 -0400

    validation

--- db/credentials.js
+++ db/credentials.js
@@ -1,10 +1,10 @@
 
 const dev = {
-    user : 'abdalla',
-    database : 'urls',
-    password: '12345678',
+    user : process.env.RDS_DB_USER || 'abdalla',
+    database : process.env.RDS_DATABASE || 'urls',
+    password: process.env.RDS_PA || '12345678',
     host: 'urls.cbs6yy5r1pr3.us-east-2.rds.amazonaws.com',
-    port: '5432',
+    port: process.env.RDS_PORT || '5432',
 }
 
 module.exports = {
