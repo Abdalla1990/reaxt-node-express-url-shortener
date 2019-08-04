2d66e02d7f13f3dd998553b74aedbb441ec8fbcd
Abdalla <motmm0500@hotmail.com>
2019-08-04 12:31:27 -0400

    validation

--- routes/index.js
+++ routes/index.js
@@ -1,25 +1,29 @@
 var express = require('express');
 var router = express.Router();
 const { storeShortenedUrl, fetchRecordById, fetchRecordByShortUrl } = require('../db/queries');
-const { prepareDatabaseEntry, generateShortUrl } = require('../tools/helpers');
+const { prepareDatabaseEntry, generateShortUrl, isValidUrl } = require('../tools/helpers');
 
+console.log('ENV : ' , process.env);
 router.post('/shorten', (req, res) => {
     const { url_to_shorten } = req.body;
-    const generatedData = prepareDatabaseEntry(url_to_shorten);
-    
-    storeShortenedUrl(generatedData).then(data => {
-      
-        if(data.rowCount > 0) {
-            fetchRecordById(generatedData.uuid)
-            .then(record => {
-               
-                return res.status(200).send(record.rows);
-            })
-            .catch(err => res.status(400).send(err));
-        }else {
-            res.status(400).send({error: 'either data already exists or something went wrong'})
-        }
-    }).catch(e => res.status(400).send({error: e.detail}));
+    if(isValidUrl(url_to_shorten)) {
+      const generatedData = prepareDatabaseEntry(url_to_shorten);
+      storeShortenedUrl(generatedData).then(data => {
+        
+          if(data.rowCount > 0) {
+              fetchRecordById(generatedData.uuid)
+              .then(record => {
+                 
+                  return res.status(200).send(record.rows);
+              })
+              .catch(err => res.status(400).send(err));
+          }else {
+              res.status(400).send({error: 'either data already exists or something went wrong'})
+          }
+      }).catch(e => res.status(400).send({error: e.detail}));  
+    }else {
+      res.status(400).send({error: 'invalid url!'});
+    }
     
 });
 
--- tools/helpers.js
+++ tools/helpers.js
@@ -1,4 +1,5 @@
 const uuidv4 = require('uuid/v4');
+
 const prepareDatabaseEntry = original => {
     const id = uuidv4();
     const shortendUrl = generateShortUrl(id);
@@ -7,9 +8,18 @@ const prepareDatabaseEntry = original => {
     short: shortendUrl,
     original
 })}
+
 const generateShortUrl = id => `https://elasticbeanstalk-us-east-2-296990967656.s3.us-east-2.amazonaws.com/${id}`
+
+const isValidUrl = url => {
+    console.log('Validation : ', url);
+    const regexp =  /^(?:(?:https?|ftp):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/;
+    console.log('Validation : ', regexp.test(url));
+    return regexp.test(url);
+};
 
 module.exports = {
     prepareDatabaseEntry,
-    generateShortUrl
+    generateShortUrl,
+    isValidUrl,
 }
